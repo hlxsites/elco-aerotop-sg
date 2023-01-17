8816a123f1bd6ead10254ac725376e092eb50eb8
alvaro <alvaro@macbook-pro-3.eur.adobe.com>
2023-01-17 15:41:04 +0100

    Fixed lint issues

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -66,7 +66,6 @@ function buildAutoBlocks(main) {
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
   // hopefully forward compatible button decoration
- // decorateButtons(main);
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
