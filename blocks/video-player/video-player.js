0208f3e9816da3d2858c22192cfe47aa183022e1
Damian Zehnder <zehnder@adobe.com>
2023-01-19 18:24:29 +0100

    add close button for desktop

--- blocks/video-player/video-player.js
+++ blocks/video-player/video-player.js
@@ -1,7 +1,37 @@
-import { loadVideo } from '../../scripts/delayed.js';
+function loadVideo(videoURL, block) {
+  const VIDEO_HOST = 'https://www.youtube.com';
+  const overlay = document.createElement('div');
+  overlay.classList.add('video-player-overlay');
+  block.parentElement.appendChild(overlay);
+
+  const toolbar = document.createElement('div');
+  toolbar.classList.add('video-player-toolbar');
+  const toolbarClose = document.createElement('div');
+  toolbarClose.classList.add('video-player-close');
+  toolbar.appendChild(toolbarClose);
+  block.parentElement.appendChild(toolbar);
+
+  const videoIframe = document.createElement('iframe');
+  videoIframe.classList.add('video-player-iframe');
+  videoIframe.src = `${VIDEO_HOST}/embed${videoURL.pathname}`;
+
+  block.appendChild(videoIframe);
+
+  overlay.addEventListener('click', () => {
+    block.parentElement.removeChild(overlay);
+    block.parentElement.removeChild(toolbar);
+    block.removeChild(videoIframe);
+  });
+  toolbarClose.addEventListener('click', () => {
+    block.parentElement.removeChild(overlay);
+    block.parentElement.removeChild(toolbar);
+    block.removeChild(videoIframe);
+  });
+}
 
 export default async function decorate(block) {
   const videoURL = new URL(block.getElementsByTagName('a')[0].innerText);
 
   block.addEventListener('click', () => loadVideo(videoURL, block));
+
 }
