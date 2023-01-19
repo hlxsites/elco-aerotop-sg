d64de2e7c0597df08663dc032ff1f299a85ce5ae
Damian Zehnder <zehnder@adobe.com>
2023-01-19 15:37:46 +0100

    move loadVideo to delayed

--- blocks/video-player/video-player.js
+++ blocks/video-player/video-player.js
@@ -1,22 +1,4 @@
-const VIDEO_HOST = 'https://www.youtube.com';
-
-function loadVideo(videoURL, block) {
-  const overlay = document.createElement('div');
-  overlay.classList.add('video-player-overlay');
-
-  block.parentElement.appendChild(overlay);
-
-  const videoIframe = document.createElement('iframe');
-  videoIframe.classList.add('video-player-iframe');
-  videoIframe.src = `${VIDEO_HOST}/embed${videoURL.pathname}`;
-
-  block.appendChild(videoIframe);
-
-  overlay.addEventListener('click', () => {
-    block.parentElement.removeChild(overlay);
-    block.removeChild(videoIframe);
-  });
-}
+import { loadVideo } from '../../scripts/delayed.js';
 
 export default async function decorate(block) {
   const videoURL = new URL(block.getElementsByTagName('a')[0].innerText);
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,23 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// eslint-disable-next-line import/prefer-default-export
+export function loadVideo(videoURL, block) {
+  const VIDEO_HOST = 'https://www.youtube.com';
+  const overlay = document.createElement('div');
+  overlay.classList.add('video-player-overlay');
+
+  block.parentElement.appendChild(overlay);
+
+  const videoIframe = document.createElement('iframe');
+  videoIframe.classList.add('video-player-iframe');
+  videoIframe.src = `${VIDEO_HOST}/embed${videoURL.pathname}`;
+
+  block.appendChild(videoIframe);
+
+  overlay.addEventListener('click', () => {
+    block.parentElement.removeChild(overlay);
+    block.removeChild(videoIframe);
+  });
+}
