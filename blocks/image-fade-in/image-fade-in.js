7eff7381e7d3d120a0818967096827be237c0dbe
alvaro <alvaro@MacBook-Pro-6.local>
2023-01-24 16:33:01 +0100

    Solved lint issues

--- blocks/image-fade-in/image-fade-in.js
+++ blocks/image-fade-in/image-fade-in.js
@@ -1,12 +1,9 @@
 function createFadeInImage(block) {
-  block.id='fade-in';
+  block.id = 'fade-in';
   const firstLevelDivs = block.querySelectorAll(':scope > div');
-  console.log(block.parentNode);
-  console.log('size es '+firstLevelDivs.length)
   firstLevelDivs.forEach((element, i) => {
-    console.log(firstLevelDivs[i]);
     firstLevelDivs[i].classList.add(`image-${i}`);
-});
+  });
 }
 
 export default async function decorate(block) {
