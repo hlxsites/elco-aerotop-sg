15b2e3864de47df1983bc181be23e1a794df41a9
Damian Zehnder <zehnder@adobe.com>
2023-01-24 14:26:55 +0100

    feat: adding navigation

--- scripts/image-lib.js
+++ scripts/image-lib.js
@@ -1,3 +1,49 @@
+const PICTURE_ID = 'image-viewer-picture';
+function generateImage(sourceElement) {
+  const picture = document.createElement('picture');
+  picture.id = PICTURE_ID;
+  Object.values(sourceElement.children).forEach((picSrc) => {
+    switch (picSrc.tagName) {
+      case 'SOURCE': {
+        const source = document.createElement('source');
+        source.type = picSrc.type;
+        source.srcset = picSrc.srcset;
+        source.media = picSrc.media;
+        picture.appendChild(source);
+        break;
+      }
+      case 'IMG': {
+        const img = document.createElement('img');
+        img.loading = picSrc.loading;
+        img.alt = picSrc.alt;
+        img.type = picSrc.type;
+        img.src = picSrc.src;
+        img.width = picSrc.width;
+        img.height = picSrc.height;
+        picture.appendChild(img);
+        break;
+      }
+      default:
+    }
+  });
+  return picture;
+}
+
+function adjustNavButtonVisibility(pictures, currentImagePos, navLeft, navRight) {
+  if (pictures.length === 1) {
+    navLeft.style.visibility = 'hidden';
+    navRight.style.visibility = 'hidden';
+  } else if (currentImagePos === pictures.length - 1) {
+    navRight.style.visibility = 'hidden';
+    navLeft.style.visibility = 'visible';
+  } else if (currentImagePos === 0) {
+    navLeft.style.visibility = 'hidden';
+    navRight.style.visibility = 'visible';
+  } else {
+    navLeft.style.visibility = 'visible';
+    navRight.style.visibility = 'visible';
+  }
+}
 export default function generateImageOverlayImpl(e, block, pictures) {
   const imageViewerWrapper = document.createElement('div');
   imageViewerWrapper.classList.add('image-viewer-wrapper');
@@ -9,51 +55,63 @@ export default function generateImageOverlayImpl(e, block, pictures) {
   imageViewerToolbar.appendChild(closeButton);
   const imageViewerInfobar = document.createElement('div');
   imageViewerInfobar.classList.add('image-viewer-infobar');
-  imageViewerInfobar.innerText = `${[...e.currentTarget.parentElement.children].indexOf(e.currentTarget) + 1} / ${pictures.length}`;
+  let currentImagePos = [...e.currentTarget.parentElement.children].indexOf(e.currentTarget);
+  imageViewerInfobar.innerText = `${currentImagePos + 1} / ${pictures.length}`;
 
   const imageViewerBackground = document.createElement('div');
   imageViewerBackground.classList.add('image-viewer-background');
 
+  const imageViewerNav = document.createElement('div');
+
+  const navLeft = document.createElement('button');
+  navLeft.classList.add('image-viewer-nav-left');
+  imageViewerNav.appendChild(navLeft);
+  imageViewerNav.classList.add('image-viewer-nav');
+  const navRight = document.createElement('button');
+  navRight.classList.add('image-viewer-nav-right');
+  imageViewerNav.appendChild(navRight);
+  adjustNavButtonVisibility(pictures, currentImagePos, navLeft, navRight);
+
   const imageViewer = document.createElement('div');
   imageViewer.classList.add('image-viewer-image');
 
+  navLeft.addEventListener('click', () => {
+    currentImagePos -= 1;
+
+    // switch image
+    adjustNavButtonVisibility(pictures, currentImagePos, navLeft, navRight);
+    document.getElementById(PICTURE_ID).remove();
+    const picture = generateImage(pictures[currentImagePos]);
+    imageViewer.appendChild(picture);
+
+    // update infobar
+    imageViewerInfobar.innerText = `${currentImagePos + 1} / ${pictures.length}`;
+  });
+
+  navRight.addEventListener('click', () => {
+    currentImagePos += 1;
+
+    // switch image
+    adjustNavButtonVisibility(pictures, currentImagePos, navLeft, navRight);
+    document.getElementById(PICTURE_ID).remove();
+    const picture = generateImage(pictures[currentImagePos]);
+    imageViewer.appendChild(picture);
+
+    // update infobar
+    imageViewerInfobar.innerText = `${currentImagePos + 1} / ${pictures.length}`;
+  });
+
   if (e.currentTarget.tagName === 'PICTURE') {
-    const picture = document.createElement('picture');
-    Object.values(e.currentTarget.children).forEach((picSrc) => {
-      switch (picSrc.tagName) {
-        case 'SOURCE': {
-          const source = document.createElement('source');
-          source.type = picSrc.type;
-          source.srcset = picSrc.srcset;
-          source.media = picSrc.media;
-          picture.appendChild(source);
-          break;
-        }
-        case 'IMG': {
-          const img = document.createElement('img');
-          img.loading = picSrc.loading;
-          img.alt = picSrc.alt;
-          img.type = picSrc.type;
-          img.src = picSrc.src;
-          img.width = picSrc.width;
-          img.height = picSrc.height;
-          picture.appendChild(img);
-          break;
-        }
-        default:
-      }
-    });
+    const picture = generateImage(e.currentTarget);
 
     imageViewer.appendChild(picture);
     imageViewerWrapper.appendChild(imageViewerInfobar);
     imageViewerWrapper.appendChild(imageViewerToolbar);
     imageViewerWrapper.appendChild(imageViewer);
+    imageViewerWrapper.appendChild(imageViewerNav);
     imageViewerWrapper.appendChild(imageViewerBackground);
     block.parentElement.appendChild(imageViewerWrapper);
 
-    imageViewerBackground.addEventListener('click', () => {
-      imageViewerWrapper.remove();
-    });
     closeButton.addEventListener('click', () => {
       imageViewerWrapper.remove();
     });
