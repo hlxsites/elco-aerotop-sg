91c1167157521107a0856c8a35c4fdb301741514
alvaro <alvaro@MacBook-Pro-6.local>
2023-01-26 10:59:11 +0100

    Functioning resizing table

--- blocks/technology-table/technology-table.js
+++ blocks/technology-table/technology-table.js
@@ -1,13 +1,9 @@
+
 function buildTechnologyTable(block) {
-    block.style.cssText = "display : flex; flex-wrap: wrap; flex-direction: column;";
     const firstDivElements = block.querySelectorAll('.technology-table > div');
     firstDivElements.forEach((element, i) => {
-        element.style.display = 'flex';
         const secondLevelDiv = element.querySelectorAll('div > div');
         secondLevelDiv.forEach((subelement, j) => {
-            console.log(j + ' ' + secondLevelDiv.length);
-            if (j < secondLevelDiv.length - 1) subelement.style.cssText = "flex: 1 1 10%; margin-right: 4%";
-            else subelement.style.cssText = "flex: 1 1 10%;";
             if (i === 0) {
                 const TextInDiv = subelement.textContent;
                 subelement.textContent = '';
@@ -17,12 +13,72 @@ function buildTechnologyTable(block) {
                 title.style.textAlign = 'left';
                 subelement.appendChild(title);
             }
+            if (i === 0) {
+                subelement.classList.add('techno-table-title-' + j);
+            } else if (i === 1) {
+                subelement.classList.add('techno-table-image-' + j);
+            } else if (i === 2){
+                subelement.classList.add('techno-table-description-' + j);
+            }
+        });
+    });
+}
+
+function createTableForMobile(block) {
+const newTableTechnoForMobile = document.createElement('div');
+newTableTechnoForMobile.classList.add('table-techno-mobile');
+block.parentElement.appendChild(newTableTechnoForMobile);
+
+const titleTables=[];
+const imageTables=[];
+const descriptionTables=[];
+
+const firstDivElements = block.querySelectorAll('.technology-table > div');
+    firstDivElements.forEach((element, i) => {
+        const secondLevelDiv = element.querySelectorAll('div > div');
+        secondLevelDiv.forEach((subelement, j) => {
+            if (i === 0) {
+                titleTables[j]=subelement.cloneNode(true);
+            }
+            else if (i === 1) {
+                imageTables[j]=subelement.cloneNode(true);;
+            } else if (i === 2){
+                descriptionTables[j]=subelement.cloneNode(true);;
+            }
         });
     });
 
+    titleTables.forEach((theTitle,i) =>  {
+        
+        const TextInDiv = titleTables[i].textContent;
+        titleTables[i].textContent = '';
+        const titleH1 = document.createElement("h1");
+        titleH1.textContent = TextInDiv;
+        titleH1.style.fontSize = 'var(--heading-font-size-l)';
+        titleH1.style.textAlign = 'left';
+        titleTables[i].appendChild(titleH1);
+        titleTables[i].style.margin='0 0 20px 0';
+
+        newTableTechnoForMobile.appendChild(titleTables[i]);
+        newTableTechnoForMobile.appendChild(imageTables[i]);
+        descriptionTables[i].style.margin='0 0 20px 0';
+        newTableTechnoForMobile.appendChild(descriptionTables[i]);
+        console.log(theTitle);
+    });
+
+    imageTables.forEach((theImage) =>  {
+        console.log(theImage);
+    });
+
+    descriptionTables.forEach((theDescription) =>  {
+        console.log(theDescription);
+    });
 
 }
 
+
 export default async function decorate(block) {
     buildTechnologyTable(block);
+    createTableForMobile(block);
+    block.parentElement.appendChild(document.createElement('div'));
 }
