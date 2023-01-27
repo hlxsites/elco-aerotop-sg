c561b6f3711bbfc6c660fef04fc97581dc52038d
ekdogan <ekdogan@adobe.com>
2023-01-27 15:29:46 +0300

    feat: explosion slider animation mobile compatible

--- blocks/explosion-slider/explosion-slider.js
+++ blocks/explosion-slider/explosion-slider.js
@@ -1,46 +1,52 @@
-function animateLid(lid, ratio) {
-  // ratio: 0.6 > margin-top: 12%
-  // ratio: 0.4 > margin-top: 6%
-  // 1 step = 6 / (0.6 - 0.4) = 30
-  if (ratio > 0.6) {
-    lid.style.marginTop = '12%';
-  } else if (ratio < 0.4) {
-    lid.style.marginTop = '6%';
+/**
+ * animates vent and lid sprites
+ * @param sprite DOM element to animate
+ * @param height height of the container in DOM (in px)
+ * @param visible visible part of the container (in px)
+ * @param from [0, 1] percentage of the DOM element visible on the screen to start animation
+ * @param to [0, 1] percentage of the DOM element visible on the screen to end animation
+ * @param minMargin min margin value of the DOM element at the beginning of animation
+ * @param maxMargin max margin value of the DOM element at the beginning of animation
+ */
+function animateSprite(sprite, height, visible, from, to, minMargin, maxMargin) {
+  const startAnimate = height * from;
+  const stopAnimate = height * to;
+
+  const step = (maxMargin - minMargin) / (stopAnimate - startAnimate);
+
+  if (visible < startAnimate) {
+    sprite.style.marginTop = `${maxMargin}%`;
+  } else if (visible > stopAnimate) {
+    sprite.style.marginTop = `${minMargin}%`;
   } else {
-    lid.style.marginTop = `${(ratio - 0.4) * 30 + 6}%`;
+    const overlap = visible - startAnimate;
+    sprite.style.marginTop = `${maxMargin - overlap * step}%`;
   }
 }
 
-function animateVent(lid, ratio) {
-  // ratio: 0.6 > margin-top: 7%
-  // ratio: 0.4 > margin-top: 4%
-  // 1 step = 3 / (0.6 - 0.4) = 15
-  if (ratio > 0.6) {
-    lid.style.marginTop = '7%';
-  } else if (ratio < 0.4) {
-    lid.style.marginTop = '4%';
-  } else {
-    console.log(ratio);
-    lid.style.marginTop = `${(ratio - 0.4) * 15 + 4}%`;
-  }
-}
+/**
+ * animates text elements' opacity
+ * @param text DOM element to animate its opacity
+ * @param height height of the container in DOM (in px)
+ * @param visible visible part of the container (in px)
+ * @param from [0, 1] percentage of the container visible on the screen to start animation
+ * @param to [0, 1] percentage of the container visible on the screen to end animation
+ * @param minOpacity min opacity value of the DOM element at the beginning of animation
+ * @param maxOpacity max opacity value of the DOM element at the beginning of animation
+ */
+function animateText(text, height, visible, from, to, minOpacity, maxOpacity) {
+  const startAnimate = height * from;
+  const stopAnimate = height * to;
 
-function animateTexts(texts, ratio) {
-  // ratio: 0.4 > opacity: 0
-  // ratio: 0.33 > opacity: 1
-  // 1 step = 1 / (0.4 - 0.33) = 14
-  if (ratio > 0.4) {
-    texts.forEach((text) => {
-      text.style.opacity = 0;
-    });
-  } else if (ratio < 0.33) {
-    texts.forEach((text) => {
-      text.style.opacity = 1;
-    });
+  const step = (maxOpacity - minOpacity) / (stopAnimate - startAnimate);
+
+  if (visible < startAnimate) {
+    text.style.opacity = `${minOpacity}`;
+  } else if (visible > stopAnimate) {
+    text.style.opacity = `${maxOpacity}`;
   } else {
-    texts.forEach((text) => {
-      text.style.opacity = `${(0.4 - ratio) * 14}`;
-    });
+    const overlap = visible - startAnimate;
+    text.style.opacity = `${overlap * step + minOpacity}`;
   }
 }
 
@@ -50,13 +56,16 @@ export default async function decorate(block) {
   const texts = block.querySelectorAll(':scope > div:nth-child(n+4)');
 
   window.addEventListener('scroll', () => {
-    const { top, height } = block.getBoundingClientRect();
-    if (top > 0 && top < height) {
-      const ratio = top / height;
+    const { top, width, bottom } = block.getBoundingClientRect();
+    const height = width * 1875 / 2500;
 
-      animateLid(lid, ratio);
-      animateVent(vent, ratio);
-      animateTexts(texts, ratio);
+    if(top < window.innerHeight && bottom >= 0) {
+      const visible = window.innerHeight - top;
+      animateSprite(lid, height, visible, 0.3, 0.5, 6, 12);
+      animateSprite(vent, height, visible, 0.3, 0.5, 4, 7);
+      Array.from(texts).forEach((text) => {
+        animateText(text, height, visible, 0.5, 0.6, 0, 1);
+      });
     }
   }, false);
 }
