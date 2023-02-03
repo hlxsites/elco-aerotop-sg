8cc0a7d1086835c3972abbd14adac1484bb844b3
ekdogan <ekdogan@adobe.com>
2023-02-03 17:36:07 +0100

    feat: martech integrations back to delayed

--- scripts/martech-integrations.js
+++ scripts/martech-integrations.js
@@ -16,6 +16,7 @@ const HOTJAR_SCRIPT = `(function(h,o,t,j,a,r){
     a=o.getElementsByTagName('head')[0];
     r=o.createElement('script');r.async=1;
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    r.type="application/javascript";
     a.appendChild(r);
   })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
 
