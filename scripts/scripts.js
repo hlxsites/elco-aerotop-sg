f97aac872dd0b85db873de4c05a41dcd1f1596ee
ekdogan <ekdogan@adobe.com>
2023-02-03 17:00:47 +0100

    feat: martech integrations back to delayed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -171,7 +171,7 @@ async function loadLazy(doc) {
  */
 function loadDelayed() {
   // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000)
+  window.setTimeout(() => import('./delayed.js'), 5000)
 
   // load anything that can be postponed to the latest here
 }
